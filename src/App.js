2cf9c01c79477e8bd1ad4b04454b1bb7a883c311
fred.aklamanu@gmail.com <fred.aklamanu@gmail.com>
2021-11-09 15:38:20 +0100

    Hello ReactApp

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hi from Julia and Fred. We are here!!!!!!!!
         </a>
       </header>
     </div>
